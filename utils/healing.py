c03f6276d6d4e39a2adb8c5f3086bb61081d04c1
Rowshnick <127508073+Rowshnick@users.noreply.github.com>
2025-11-20 02:20:28 +0330

    Update healing.py

--- utils/healing.py
+++ utils/healing.py
@@ -1,1 +1,20 @@
-def select(goal): return {'goal':goal}
+def select(goal):
+    mapping = {
+        'wealth': {
+            'stones':[{'en':'Citrine','fa':'سیترین'}],
+            'herbs':[{'en':'Bay Leaf','fa':'برگ بو'}]
+        },
+        'love': {
+            'stones':[{'en':'Rose Quartz','fa':'کوارتز رز'}],
+            'herbs':[{'en':'Rose','fa':'گل رز'}]
+        },
+        'health': {
+            'stones':[{'en':'Amethyst','fa':'آمتیست'}],
+            'herbs':[{'en':'Lavender','fa':'اسطوخودوس'}]
+        },
+        'career': {
+            'stones':[{'en':'Hematite','fa':'هماتیت'}],
+            'herbs':[{'en':'Cedar','fa':'سدر'}]
+        }
+    }
+    return mapping.get(goal, mapping['wealth'])
